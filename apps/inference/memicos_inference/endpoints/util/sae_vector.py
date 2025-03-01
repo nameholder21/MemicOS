260e4717c763e1459a3795507422d4cc0eadeb42
nameholder21 <192949959+nameholder21@users.noreply.github.com>
2025-02-28 22:00:09 -0800

    init

---
+++ apps/inference/memicos_inference/endpoints/util/sae_vector.py
@@ -0,0 +1,32 @@
+import logging
+from memicos_inference_client.models.util_sae_vector_post_request import (
+    UtilSaeVectorPostRequest,
+)
+from memicos_inference_client.models.util_sae_vector_post200_response import (
+    UtilSaeVectorPost200Response,
+)
+from memicos_inference.shared import (
+    with_request_lock,
+)
+from memicos_inference.sae_manager import SAEManager
+from fastapi import APIRouter
+
+logger = logging.getLogger(__name__)
+
+router = APIRouter()
+
+
+@router.post("/util/sae-vector")
+@with_request_lock()
+async def sae_vector(request: UtilSaeVectorPostRequest):
+
+    source = request.source
+    index = request.index
+
+    sae = SAEManager.get_instance().get_sae(source)
+
+    result = sae.W_enc[:, index].detach().tolist()
+
+    logger.info("Returning result: %s", result)
+
+    return UtilSaeVectorPost200Response(vector=result)
