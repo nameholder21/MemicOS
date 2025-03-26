b02ca2fd2bca6b0454147fbd1e21e7a0936f9632
nameholder21 <192949959+nameholder21@users.noreply.github.com>
2025-03-25 23:56:16 -0500

    webapp: update playwright tests

---
+++ apps/webapp/tests-playwright/production/account.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+// test('user log in', async ({ page }) => {
+//     await page.goto('https://memicos.org');
+
+//     // Click on the login button
+//     await page.getByRole('button', { name: 'Sign In' }).click();
+   
+//     await page.fill('email', 'memicostest@gmail.com');
+//     await page.fill('#password', 'autotest1234');
+// });
